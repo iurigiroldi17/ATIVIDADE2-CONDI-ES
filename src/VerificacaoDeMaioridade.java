6cf8d8960508d20e5cd998b7637cb1fffe75b975
iurigiroldi17 <iuri241206@edu.unipar.br>
2024-08-28 20:14:43 -0300

    Atividade2

--- src/VerificacaoDeMaioridade.java
+++ src/VerificacaoDeMaioridade.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class VerificacaoDeMaioridade {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
 
-    public static void main(String[] args) {
+            // Solicita a idade da pessoa
+            System.out.println("Digite a sua idade:");
+            int idade = scanner.nextInt();
 
+            // Verifica se a pessoa é maior de idade
+            if (idade >= 18) {
+                System.out.println("Você é maior de idade.");
+            } else {
+                System.out.println("Você é menor de idade.");
+            }
+
+            // Fecha o scanner
+            scanner.close();
+        }
     }
-}
+
