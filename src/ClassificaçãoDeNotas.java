61527e0373bd574ed1051970a57b7bfe6541cafe
iurigiroldi17 <iuri241206@edu.unipar.br>
2024-08-28 20:28:29 -0300

    Atividade2

--- src/ClassificaçãoDeNotas.java
+++ src/ClassificaçãoDeNotas.java
@@ -1,3 +1,23 @@
-public class ClassificaçãoDeNotas {
+import java.util.Scanner;
 
+public class ClassificaçãoDeNotas {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        //Solicitar nota do aluno
+        System.out.println("Digite a nota do aluno: ");
+        double nota = scanner.nextDouble();
+        if (nota == 10) {
+            System.out.println("Excelente");
+        } else if (nota >= 7) {
+            System.out.println("Bom");
+        } else if (nota >= 5) {
+            System.out.println("Satisfatório");
+        } else {
+            System.out.println("Insatisfatório");
+        }
+
+        scanner.close();
+
+    }
 }
